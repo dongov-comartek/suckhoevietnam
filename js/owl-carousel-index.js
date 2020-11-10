16c6373207798111db99794e845688f1fc5f475b
dong-comartek <dong.ong@comartek.vn>
2020-11-10 22:34:46 +0700

    create menu + footer + trending

---
+++ js/owl-carousel-index.js
@@ -0,0 +1,14 @@
+$('.owl-carousel').owlCarousel({
+   
+    loop:true,
+    margin:10,
+    nav:true,
+    dots:false,
+     autoplay: true,
+            smartSpeed: 1500,
+    responsive:{
+        0:{
+            items:1
+        }
+    }
+})
